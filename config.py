7801eab85764cd1f4912b29ffaeab8998aa4c9c7
Emiliano Ali Kick Ramirez <gloosard@gmail.com>
2024-09-19 08:22:54 -0600

    p6 aql

--- config.py
+++ config.py
@@ -1,3 +1,17 @@
 class Config:
     SECRET_KEY  =   "1$!afgs%#Asd(alñad%$#lña@asd3"
+    DEBUG = True
     
+class DevelopmentConfig:
+    MYSQL_HOST  = "localhost"
+    MYSQL_USER  = "root"
+    MSQL_PASSWORD   = ""
+    MYSQL_DB    = "kickshop"
+
+diccionario ={
+    "MYSQL_HOST"  : "localhost",
+    "MYSQL_USER"  : "root",
+    "MSQL_PASSWORD"   : "",
+    "MYSQL_DB"    : "kickshop"}
+
+print(diccionario["MYSQL_DB"])
