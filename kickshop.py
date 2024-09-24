2be6951ed5ecce8086842091855e038fd257a745
Emiliano Ali Kick Ramirez <gloosard@gmail.com>
2024-09-24 12:03:42 -0600

    p7 config

--- kickshop.py
+++ kickshop.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template,url_for,request,redirect
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
-import datetime
+from datetime import datetime 
+from config import config
 
 kickshop = Flask(__name__)
 db      = MySQL(kickshop)
@@ -16,19 +17,22 @@ def signup():
         nombre= request.form["nombre"]
         correo= request.form["correo"]
         clave=request.form["clave"]
-        clavei_cifrada=generate_password_hash(clave)
-        fechareg=datetime.datetime()
+        clave_cifrada=generate_password_hash(clave)
+        fechareg=datetime.now()
         reg_usuario=db.connection.cursor()
-        reg_usuario.execute("INSERT INTO usuario(nombre,correo,clave,fechareg)  VALUES(%s,%s,%s,%s) ",(nombre,correo,clavei_cifrada,fechareg))
-        db.connection.Commit()  
+        reg_usuario.execute("INSERT INTO usuario(nombre,correo,clave,fechareg)  VALUES(%s,%s,%s,%s) ",(nombre,correo,clave_cifrada,fechareg))
+        db.connection.commit()  
         return render_template('home.html')
     else:
         
         return render_template('signup.html')
 
 
+
 @kickshop.route("/signin")
 def signin():
     return render_template('signin.html')
-if __name__ == '__main__':  
-   kickshop.run(port=3300, debug=True) 
+
+if __name__ == '__main__':
+   kickshop.config.from_object(config["development"])
+   kickshop.run(port=3300) 
